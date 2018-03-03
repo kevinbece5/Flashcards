6831ebc228d4dee51d1cdb7a8a2d47926606180f
kevinbece5 <kevinbece5@gmail.com>
2018-03-02 18:43:55 -0600

    removed the error stack from the error.pug file because stack traces would be too long and clutter up the page

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//The modules that will be used for the application 
 const express = require('express');
 const bodyParser = require('body-parser');
 const session = require('express-session');
@@ -7,27 +8,29 @@ const mongoose = require('mongoose');
 
 const app = express();
 
+//creates the DB
 mongoose.connect('mongodb://localhost:27017/flashcards')
 const db = mongoose.connection;
 
+//error handler
 db.on('error', console.error.bind(console, 'connection error:'));
 
-db.once('open', function() {
-  console.log('Up and running');
-});
 
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(cookieParser());
 app.use('/static', express.static('public'));
 
+//Sets the view engine to PUG
 app.set('view engine', 'pug');
 
 const mainRoutes = require('./routes');
 const cardRoutes = require('./routes/cards');
 
+//Directs to the routes file that will handle the paths for the application
 app.use(mainRoutes);
 app.use('/cards', cardRoutes);
 
+//Error middleware for 404
 app.use((req, res, next) => {
   const err = new Error('Not Found');
   err.status = 404;
@@ -39,6 +42,7 @@ app.use((err, req, res, next) => {
   res.status(err.status);
   res.render('error');
 });
+
 
 app.listen(3000, () => {
     console.log('The application is running on localhost:3000!')
